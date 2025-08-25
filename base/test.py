189cda92e07587df326724a5985114008851c43b
wired87 <derbenedikt.sterra@gmail.com>
2025-08-25 11:35:56 +0200

    erstellungsprozess überarbeitet

--- base/test.py
+++ base/test.py
@@ -17,6 +17,5 @@ class TestBase(RayAdminBase):
         self.init_ray()
 
         self.start_serve(namespace)
-        self.create_head_server()
         self.host["HEAD"] = serve.get_deployment_handle(ENV_ID, app_name=ENV_ID)
 
